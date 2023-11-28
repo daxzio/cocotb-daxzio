ae7a8b63a6a365b8e87bda4fb0162c4b6f0d3afe
Dave Keeshan <dave.keeshan@daxzio.com>
2023-11-28 06:08:22 -0500

    Add exisiting work

---
+++ cocotbext/daxzio/syncdriver.py
@@ -0,0 +1,50 @@
+from cocotb import start_soon
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
+
+class syncDriver:
+    def __init__(self, vsync=None, rsync=None, gsync=None, bsync=None, vsync_freq=60, offset_start=10):
+        self.vsync = vsync
+        self.vsync_freq = vsync_freq
+        self.vsync_delay = 1000000000/self.vsync_freq
+        #self.vsync.setimmediatevalue(0x0)
+        start_soon(self._vsync())
+        
+        self.rgbsync = [rsync, gsync, bsync]
+        self.offset_start = offset_start
+
+        self.t0_delay = [5000, 5000, 5000]
+        self.t1_delay = [100000000/self.vsync_freq, 100000000/self.vsync_freq, 100000000/self.vsync_freq]
+        
+        for i, x in enumerate(self.rgbsync):
+            #self.rgbsync[i].setimmediatevalue(0x0)
+            start_soon(self._rgbsync(i))
+        
+    async def _vsync(self):
+        self.vsync.value = 0
+        v0_delay = self.offset_start
+        v1_delay = 500*80
+        v2_delay = round(self.vsync_delay, 3) - v1_delay
+        await Timer(v0_delay, units='ns')
+        while True:
+            self.vsync.value = 1
+            await Timer(v1_delay, units='ns')
+            self.vsync.value = 0
+            await Timer(v2_delay, units='ns')
+
+    async def _rgbsync(self, num=0):
+        self.rgbsync[num].value = 0
+        t0_delay = self.t0_delay[num]
+        t1_delay = round(self.t1_delay[num], 0)
+        t2_delay = round(self.vsync_delay/2 - t1_delay, 3)
+        t0_delay += round(num*(self.vsync_delay/6), 3)
+        while True:
+            await RisingEdge(self.vsync)
+            await FallingEdge(self.vsync)
+            await Timer(t0_delay, units='ns')
+            for i in range(2):
+                self.rgbsync[num].value = 1
+                await Timer(t1_delay, units='ns')
+                self.rgbsync[num].value = 0
+                if 0 == i:
+                    await Timer(t2_delay, units='ns')
+
---
+++ cocotbext/daxzio/uart_bypass.py
@@ -0,0 +1,126 @@
+import logging
+from cocotb.triggers import RisingEdge
+from cocotbext.uart import UartSource, UartSink
+from clio.interfaces.clio import Clio
+
+class UartBypass:
+    
+    def __init__(self, dut, clk, uart_in='uart_txd_in', uart_out='uart_rxd_out'):
+        self.log = logging.getLogger(f"cocotb.UartBypass")
+        self.clk = clk
+        self.uart_disable = dut.uart_disable
+        self.wfifo_wr_en = dut.wfifo_wr_en
+        self.wfifo_din = dut.wfifo_din
+        self.rfifo_rd_en = dut.rfifo_rd_en
+        self.rfifo_dout = dut.rfifo_dout
+        self.rfifo_empty = dut.rfifo_empty
+        self.rfifo_full = dut.rfifo_full
+
+        self.uart_disable.setimmediatevalue(0)
+        self.wfifo_wr_en.setimmediatevalue(0)
+        self.wfifo_din.setimmediatevalue(0)
+        self.rfifo_rd_en.setimmediatevalue(0)
+        self.enable_logging()
+        self.fake_delay = 8
+
+        self.uart_source = UartSource(getattr(dut, uart_in), baud=230400, bits=8)
+        self.uart_sink   = UartSink(getattr(dut, uart_out), baud=230400, bits=8)
+        
+        self.clio = Clio()
+
+    def enable_logging(self):
+        self.log.setLevel(logging.DEBUG)
+    
+    def disable_logging(self):
+        self.log.setLevel(logging.WARNING)
+    
+    @property
+    def bytes(self):
+        return self.clio.bytes
+    
+    @property
+    def data(self):
+        return self.clio.data
+    
+    @property
+    def length(self):
+        return self.clio.length
+    
+    @property
+    def return_bytes(self):
+        return self.clio.return_bytes
+    
+    @property
+    def bytearray(self):
+        return self.clio.bytearray
+    
+    def gen_write(self, *args, **kwargs):
+        return self.clio.gen_write(*args, **kwargs)
+
+    def gen_read(self, *args, **kwargs):
+        return self.clio.gen_read(*args, **kwargs)
+
+    async def disable_uart(self):
+        await RisingEdge(self.clk)
+        self.uart_disable.value = 1
+        await RisingEdge(self.clk)
+    
+    async def enable_uart(self):
+        await RisingEdge(self.clk)
+        self.uart_disable.value = 0
+        await RisingEdge(self.clk)
+
+    async def tx_bytes(self):
+        if 0 == self.uart_disable.value:
+            await self.uart_source.write(self.bytearray)
+            await self.uart_source.wait()
+        else:
+            for x in self.bytes:
+                await RisingEdge(self.clk)
+                #self.log.debug(f"0x{x:02x}")
+                self.wfifo_wr_en.value = 1
+                self.wfifo_din.value = x
+                await RisingEdge(self.clk)
+                self.wfifo_wr_en.value = 0
+                self.wfifo_din.value = 0
+                for i in range(self.fake_delay):
+                    await RisingEdge(self.clk)
+    
+    async def rx_bytes(self):
+        if 0 == len(self.return_bytes):
+            return
+        self.returned_val = 0
+        
+        if 0 == self.uart_disable.value:
+            for j, x in enumerate(self.return_bytes):
+                data = await self.uart_sink.read()
+                #self.log.debug(data)
+                self.returned_val |= (int.from_bytes(data) << (8*j))
+        else:
+            while 1 == self.rfifo_empty.value:
+                await RisingEdge(self.clk)
+            j = 0
+            while 0 == self.rfifo_empty.value:
+                self.rfifo_rd_en.value = 1
+                await RisingEdge(self.clk)
+                self.rfifo_rd_en.value = 0
+                await RisingEdge(self.clk)
+                self.returned_val |= (self.rfifo_dout.value << (8*j))
+                for i in range(self.fake_delay):
+                    await RisingEdge(self.clk)
+                j += 1
+    
+    async def write(self, addr, data, length=None):
+        self.log.debug(f"Write 0x{addr:08x}: 0x{data:08x}")
+        self.gen_write(addr, data, length)
+        await self.tx_bytes()
+        await self.rx_bytes()
+
+    async def read(self, addr, data=None, length=None):
+        self.gen_read(addr, data, length)
+        await self.tx_bytes()
+        await self.rx_bytes()
+            
+        self.log.debug(f"Read  0x{addr:08x}: 0x{self.returned_val:08x}")
+        if not self.returned_val == self.data and not None == self.data:
+            raise Exception(f"Expected 0x{self.data:08x} doesn't match returned 0x{self.returned_val:08x}")
