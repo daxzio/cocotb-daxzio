c2871fa3b6adcaa0b09378d9756d451590d63cee
Dave Keeshan <dave.keeshan@daxzio.com>
2024-08-19 09:08:23 -0400

    Make clock reset more discrete

--- cocotbext/daxzio/clkreset.py
+++ cocotbext/daxzio/clkreset.py
@@ -2,32 +2,76 @@ from cocotb.clock import Clock
 from cocotb import start_soon
 from cocotb.triggers import RisingEdge, Timer
 
-class ClkReset:
-    def __init__(self, dut, period=10, clk_freq=None, reset_length=100, reset_sense=1, clkname='clk', resetname='reset'):
+class Clk:
+    def __init__(self, dut, period=10, units="ns", clkname='clk'):
         self.clk = getattr(dut, clkname)
-        self.reset = getattr(dut, resetname)
-        self.clk_freq = clk_freq
-        if not self.clk_freq is None:
-            self.period = 1000/self.clk_freq
-        else:
-            self.period = period
-        #print(f"clock period {self.period} {self.clk_freq}")
-        self.reset_length = reset_length
-        self.reset_sense = reset_sense
-        
-        start_soon(self.start_test(self.period, reset_length=self.reset_length))
+        self.period = period
+        start_soon(Clock(self.clk, self.period, units=units).start())        
 
     async def wait_clkn(self, length=1):
         for i in range(int(length)):
             await RisingEdge(self.clk)
 
-    async def start_test(self, period=10, units="ns", reset_length=100):
-        start_soon(Clock(self.clk, self.period, units=units).start())        
-        
-        self.reset.setimmediatevalue(self.reset_sense)
-        await self.wait_clkn(reset_length)
-        self.reset.value = (~self.reset_sense)  & 0x1
-        await self.wait_clkn(100)
+class Reset:
+    def __init__(self, dut, clk, reset_length=100, reset_sense=1, resetname='reset'):
+        self.reset = getattr(dut, resetname)
+        self.reset_length = reset_length
+        self.reset_sense = reset_sense
+        self.clk = clk
+        self.finished = False
 
-    async def end_test(self, length=10):
-        await self.wait_clkn(length)
+        self.reset.setimmediatevalue(self.reset_sense)
+        start_soon(self.set_reset())
+    
+    async def set_reset(self):
+        self.finished = False
+        await self.clk.wait_clkn(self.reset_length)
+        self.reset.value = (~self.reset_sense)  & 0x1
+        self.finished = True
+
+class ClkReset:
+   def __init__(self, dut, period=10, clk_freq=None, reset_length=100, reset_sense=1, clkname='clk', resetname='reset'):
+       self.clk_freq = clk_freq
+       if not self.clk_freq is None:
+           self.period = 1000/self.clk_freq
+       else:
+           self.period = period
+#        #print(f"clock period {self.period} {self.clk_freq}")
+       self.clk = Clk(dut, period=self.period, clkname=clkname)
+       self.reset = Reset(dut, self.clk, reset_length=reset_length, reset_sense=reset_sense, resetname=resetname)
+
+   async def wait_clkn(self, length=1):
+       await self.clk.wait_clkn(length)
+
+   async def end_test(self, length=10):
+       await self.wait_clkn(length)
+
+# class ClkReset:
+#    def __init__(self, dut, period=10, clk_freq=None, reset_length=100, reset_sense=1, clkname='clk', resetname='reset'):
+#         self.clk = getattr(dut, clkname)
+#         self.reset = getattr(dut, resetname)
+#         self.clk_freq = clk_freq
+#         if not self.clk_freq is None:
+#             self.period = 1000/self.clk_freq
+#         else:
+#             self.period = period
+#         #print(f"clock period {self.period} {self.clk_freq}")
+#         self.reset_length = reset_length
+#         self.reset_sense = reset_sense
+#         
+#         start_soon(self.start_test(self.period, reset_length=self.reset_length))
+# 
+#     async def wait_clkn(self, length=1):
+#         for i in range(int(length)):
+#             await RisingEdge(self.clk)
+# 
+#     async def start_test(self, period=10, units="ns", reset_length=100):
+#         start_soon(Clock(self.clk, self.period, units=units).start())        
+#         
+#         self.reset.setimmediatevalue(self.reset_sense)
+#         await self.wait_clkn(reset_length)
+#         self.reset.value = (~self.reset_sense)  & 0x1
+#         await self.wait_clkn(100)
+# 
+#     async def end_test(self, length=10):
+#         await self.wait_clkn(length)
